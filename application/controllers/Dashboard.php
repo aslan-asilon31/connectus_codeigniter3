3bdeeb8cf07892731db851a1250561d1c68aab23
aslan-asilon31 <116990574+aslan-asilon31@users.noreply.github.com>
2024-01-17 08:25:18 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,12 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 
         public function index()
-        {
-          $data = array(
-            'title' => 'Your Page Title',
-            'header' => 'Welcome to Your Site',
-            'content' => 'This is the main content of your page.',
-        );
+                        $data['title']=' Dashboard Page';
+                        $data['header']='backend_layout_header';
+                        $data['footer']='backend_layout_footer';
+                        $data['content']='dashboard';
+
 
         $this->load->view('layout_backend', $data); 
         }
