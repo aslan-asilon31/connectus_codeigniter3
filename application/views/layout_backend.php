599834d90719da4c3118f8103ee4fbe5551f1bb2
aslan-asilon31 <116990574+aslan-asilon31@users.noreply.github.com>
2024-01-17 08:19:30 +0700

    Update layout_backend.php

--- application/views/layout_backend.php
+++ application/views/layout_backend.php
@@ -1,15 +1,4 @@
-<!-- application/views/template.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-</head>
-<body>
-    <header>
-        <h1><?php echo $header; ?></h1>
-    </header>
+
     <div>
         <?php echo $content; ?>
     </div>
