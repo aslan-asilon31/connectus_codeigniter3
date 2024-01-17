8c1ed994feebf2cadd25d901c5a764339c1f06b2
aslan-asilon31 <116990574+aslan-asilon31@users.noreply.github.com>
2024-01-17 07:54:13 +0700

    Update Dashboard.php

--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -1,4 +1,5 @@
-<?php
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script><?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
@@ -16,6 +17,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 </div>
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 </body>
 </html>
